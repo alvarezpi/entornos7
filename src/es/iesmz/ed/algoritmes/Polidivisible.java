923c4fbfc7616fe90e1a0170384a56d92e205b97
Pilar Alvarez <elsalvmej@alu.edu.gva.es>
2023-06-02 13:12:56 +0200

    Polidivisble Error V.2 Funciona

--- src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -1,46 +1,53 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.Scanner;
 /**
-* Esta es una clase que describe unos cables para hacer una conexion
+* Esta es una clase que determina si un numero es o no polidivisible
 * @author: Pilar Alvarez
 * @version: 02/06/2023/
  */
 public class Polidivisible {
     private long numero;
 
+/**
+* Constructor que recibe un número como parametro, y verifica si el numero es polidivisble.
+* @param numero define la cifra  que se analizará
+*/
     public Polidivisible(long numero) {
         this.numero = numero;
     }
-
+    /**
+     * Metodo que verifica si el numero tiene todos los digitos desde 0 hasta longitu -1
+     * @return la verificacion
+     */
     public boolean esPolidivisible() {
-        int longitud = String.valueOf(numero).length();
-        return esPolidivisibleRecursivo(numero, longitud);
-    }
+        String numeroStr = String.valueOf(numero);
+        int longitud = numeroStr.length();
 
-    private boolean esPolidivisibleRecursivo(long num, int longitud) {
-        if (longitud == 1) {
-            return true;
-        }
+        for (int i = 1; i <= longitud; i++) {
+            long subNumero = Long.parseLong(numeroStr.substring(0, i));
 
-        if (!tieneDigitosSecuenciales(num, longitud)) {
-            return false;
-        }
-
-        long siguienteNum = num / 10;
-        return esPolidivisibleRecursivo(siguienteNum, longitud - 1);
-    }
-
-    private boolean tieneDigitosSecuenciales(long num, int longitud) {
-        String numeroStr = String.valueOf(num);
-
-        for (int i = 0; i < longitud; i++) {
-            int digito = Character.getNumericValue(numeroStr.charAt(i));
-            if (digito != i) {
+            if (subNumero % i != 0) {
                 return false;
             }
         }
 
         return true;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLong()) {
+            long numero = scanner.nextLong();
+            Polidivisible polidivisible = new Polidivisible(numero);
+            boolean esPolidivisible = polidivisible.esPolidivisible();
+
+            if (esPolidivisible) {
+                System.out.println("POLIDIVISIBLE");
+            } else {
+                System.out.println("NOPOLIDIVISIBLE");
+            }
+        }
+        scanner.close();
     }
 
 }
